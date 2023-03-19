acb3547ceed413a17f5da594788ee8afd56a6e8e
JP <jpcodingjourney@gmail.com>
2023-03-20 00:48:21 +1100

    Updates

--- src/pages/CreatePost.jsx
+++ src/pages/CreatePost.jsx
@@ -145,8 +145,7 @@ const CreatePost = () => {
 
         <div className="mt-10">
           <p className="mt-2 text-[#666e75] text-[14px]">
-            ** Once you have created the image you want, you can share it with
-            others in the community **
+            ** You can share the image you created with the community **
           </p>
           <button
             type="submit"
